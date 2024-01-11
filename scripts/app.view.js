b126dc670bd18508ef872ecae107c674e4fe051a
MACTEPwar <shebanic95@gmail.com>
2024-01-11 20:22:31 +0200

    fix

---
+++ scripts/app.view.js
@@ -0,0 +1,16 @@
+import { NavigateService } from "./navigate.service.js";
+import { CatalogService } from "./catalog.service.js";
+import { CommentService } from "./comment.service.js";
+import { ReceiptService } from "./receipt.service.js";
+
+export class AppView {
+  providers = new Map([
+    ["receiptService", new ReceiptService()],
+    ["navigateService", new NavigateService(this, $("#content"))],
+    ["commentService", new CommentService()],
+    ["catalogService", new CatalogService()],
+    ["commentService", new CommentService()],
+  ]);
+
+  currentView;
+}
--- scripts/main.js
+++ scripts/main.js
@@ -1,33 +1,7 @@
-import { NavigateService } from "./navigate.service.js";
-import { CatalogService } from "./catalog.service.js";
-import { CommentService } from "./comment.service.js";
-import { ReceiptService } from "./receipt.service.js";
-
-export class AppView {
-  providers = new Map([
-    ["receiptService", new ReceiptService()],
-    ["navigateService", new NavigateService(this, $("#content"))],
-    ["commentService", new CommentService()],
-    ["catalogService", new CatalogService()],
-    ["commentService", new CommentService()],
-  ]);
-
-  currentView;
-}
-
-// window.commentService = new CommentService();
-// window.catalogService = new CatalogService();
-// window.receiptService = new ReceiptService();
-// window.navigateService = new NavigateService($("#content"));
-
-// export const catalogView = new CatalogView();
-
-// window.catalogView = new CatalogView();
-// window.mainView = new MainView();
-// window.cartView = new CartView();
-// window.checkoutView = new CheckoutView();
+import { AppView } from "./app.view.js";
 
 window.app = new AppView();
+
 const receiptService = app.providers.get("receiptService");
 const navigateService = app.providers.get("navigateService");
 
@@ -36,8 +10,6 @@ $(document).ready(() => {
     const page = $(e.currentTarget).attr("id");
     app.providers.get("navigateService").navigateTo(page);
   });
-
-  // loadPage("catalog");
 
   receiptService.badgeDOM = $("#badge-cart");
   navigateService.loadDefaultPage();
